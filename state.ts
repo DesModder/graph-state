e8d6334b423603e88fbafee09f1df5a387ee570e
Jared Hughes <jahughes241@gmail.com>
2021-06-19 23:43:40 -0700

    Refine Expression type until one graph passes

--- state.ts
+++ state.ts
@@ -9,15 +9,22 @@
  *     properties are required (other generators did not do this)
  *
  * Current inaccuracies:
- *  - ExpressionState
- *  - some optional keys, such as in GrapherState
+ *  - should optional booleans be `key?: true` or `key?: boolean`?
+ *  - a few others; Ctrl+F for "???"
  *  - probably a lot more
+ *
+ * Useful sources:
+ *  - core/types/*
+ *  - graphing-calc/models/*
+ *  - core/graphing-calc/json/*
  */
 export interface State {
   version: number;
   randomSeed: string;
   graph: GrapherState;
-  expressions: ListState;
+  expressions: {
+    list: ListState;
+  };
 }
 
 type ArrowMode = "NONE" | "POSITIVE" | "BOTH";
@@ -29,22 +36,22 @@ interface GrapherState {
     xmax: number;
     ymax: number;
   };
-  xAxisMinorSubdivisions: number;
-  yAxisMinorSubdivisions: number;
-  degreeMode: boolean;
-  showGrid: boolean;
-  showXAxis: boolean;
-  showYAxis: boolean;
-  xAxisNumbers: boolean;
-  yAxisNumbers: boolean;
-  polarNumbers: boolean;
-  enableTabindex: boolean;
-  xAxisStep: number;
-  yAxisStep: number;
-  xAxisArrowMode: ArrowMode;
-  yAxisArrowMode: ArrowMode;
-  xAxisLabel: string;
-  yAxisLabel: string;
+  xAxisMinorSubdivisions?: number;
+  yAxisMinorSubdivisions?: number;
+  degreeMode?: boolean;
+  showGrid?: boolean;
+  showXAxis?: boolean;
+  showYAxis?: boolean;
+  xAxisNumbers?: boolean;
+  yAxisNumbers?: boolean;
+  polarNumbers?: boolean;
+  enableTabindex?: boolean;
+  xAxisStep?: number;
+  yAxisStep?: number;
+  xAxisArrowMode?: ArrowMode;
+  yAxisArrowMode?: ArrowMode;
+  xAxisLabel?: string;
+  yAxisLabel?: string;
 }
 
 type Latex = string;
@@ -62,14 +69,119 @@ type ItemState =
 
 interface BaseItemModel {
   id: ID;
-  secret: boolean;
+  secret?: boolean;
 }
 
 interface NonfolderModel extends BaseItemModel {
-  folderId: ID;
+  folderId?: ID;
 }
 
-interface ExpressionState {}
+type LineStyle = "SOLID" | "DASHED" | "DOTTED";
+type PointStyle = "POINT" | "OPEN" | "CROSS";
+type DragMode = "NONE" | "X" | "Y" | "XY" | "AUTO";
+type LabelSize = "SMALL" | "MEDIUM" | "LARGE";
+type LabelOrientation =
+  | "default"
+  | "center"
+  | "center_auto"
+  | "auto_center"
+  | "above"
+  | "above_left"
+  | "above_right"
+  | "above_auto"
+  | "below"
+  | "below_left"
+  | "below_right"
+  | "below_auto"
+  | "left"
+  | "auto_left"
+  | "right"
+  | "auto_right";
+
+interface ExpressionState extends NonfolderModel {
+  type: "expression";
+  color: string;
+  latex: Latex;
+  showLabel?: boolean;
+  label?: string;
+  hidden?: boolean;
+  points?: boolean;
+  lines?: boolean;
+  lineStyle?: LineStyle;
+  pointStyle?: PointStyle;
+  fill?: boolean;
+  dragMode?: DragMode;
+  labelSize?: LabelSize;
+  labelOrientation?: LabelOrientation;
+  suppressTextOutline?: boolean;
+  interactiveLabel?: boolean;
+  editableLabelMode?: boolean;
+  residualVariable?: Latex;
+  regressionParameters?: {
+    // key is a LaTeX identifier
+    [key: string]: number;
+  };
+  isLogModeRegression?: boolean;
+  displayEvaluationAsFraction?: boolean;
+  slider?: {
+    hardMin?: boolean;
+    hardMax?: boolean;
+    animationPeriod?: number;
+    loopMode?:
+      | "LOOP_FORWARD_REVERSE"
+      | "LOOP_FORWARD"
+      | "PLAY_ONCE"
+      | "PLAY_INDEFINITELY";
+    playDirection?: 1 | -1;
+    isPlaying?: boolean;
+    min?: Latex;
+    max?: Latex;
+    step?: Latex;
+  };
+  polarDomain?: {
+    min: Latex;
+    max: Latex;
+  };
+  parametricDomain?: {
+    min: Latex;
+    max: Latex;
+  };
+  cdf?: {
+    show: boolean;
+    min?: Latex;
+    max?: Latex;
+  };
+  colorLatex?: Latex;
+  fillOpacity?: Latex;
+  lineOpacity?: Latex;
+  pointOpacity?: Latex;
+  pointSize?: Latex;
+  lineWidth?: Latex;
+  labelAngle?: Latex;
+  vizProps?: {
+    breadth?: Latex;
+    axisOffset?: Latex;
+    alignedAxis?: "x" | "y";
+    showBoxplotOutliers?: boolean;
+    binAlignment?: "left" | "center";
+    // the string "binned" is never actually checked,
+    // just inferred by the absence of "exact"
+    dotplotXMode?: "exact" | "binned";
+    // the string "count" is never actually checked,
+    // just inferred by the absence of "relative" and "density"
+    histogramMode?: "count" | "relative" | "density";
+  };
+  clickableInfo?: {
+    enabled: boolean;
+    // description is the screen reader label
+    description?: string;
+    rules?: {
+      id: string;
+      expression: Latex;
+      assignment: Latex;
+    }[];
+  };
+}
 
 interface ImageState extends NonfolderModel {
   type: "image";
@@ -87,9 +199,9 @@ interface TableColumn {
   hidden?: boolean;
   points?: boolean;
   lines?: boolean;
-  dragMode?: "NONE" | "X" | "Y" | "XY" | "AUTO";
-  lineStyle?: "SOLID" | "DASHED" | "DOTTED";
-  pointStyle?: "POINT" | "OPEN" | "CROSS";
+  dragMode?: DragMode;
+  lineStyle?: LineStyle;
+  pointStyle?: PointStyle;
   colorLatex?: Latex;
   lineOpacity?: Latex;
   lineWidth?: Latex;
